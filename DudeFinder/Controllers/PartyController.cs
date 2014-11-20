7f36f703836a85aa63fea10ce2cbb0b9f90940f4
Markus Keil <markus@keil-connect.com>
2014-11-20 23:35:54 +0100

    renamed controller. added mysql db. creating party works

---
+++ DudeFinder/Controllers/PartyController.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DudeFinder.Controllers
+{
+    public class PartyController : Controller
+    {
+
+
+
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Join(string id)
+        {
+            double lat = 0.0;
+            double lng = 0.0;
+
+            using (MySqlConnection sqlconn = new MySqlConnection(ConfigurationManager.ConnectionStrings["AzureDB"].ConnectionString))
+            {
+                sqlconn.Open();
+                string sql_command = "SELECT lat,lng FROM parties WHERE partyid = @id";
+                MySqlCommand cmd = new MySqlCommand(sql_command, sqlconn);
+                cmd.Parameters.AddWithValue("@id", id);
+                var mr = cmd.ExecuteReader();
+                if (!mr.Read())
+                    return RedirectToAction("NotFound");
+                else
+                {
+                    lat = mr.GetDouble(0);
+                    lng = mr.GetDouble(1);
+                    mr.Close();
+                }
+                sqlconn.Close();
+            }
+
+            ViewBag.PartyID = id;
+            ViewBag.Lat = lat;
+            ViewBag.Lng = lng;
+
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult Create(double lat,double lng)
+        {
+            
+            string partyid = MD5Gen.ConvertStringtoMD5(Guid.NewGuid().ToString());
+            ViewBag.PartyID = partyid;
+            ViewBag.PartyUrl =  VirtualPathUtility.ToAbsolute(Url.Action("Join", new { id = partyid }));
+            ViewBag.Lat = lat;
+            ViewBag.Lng = lng;
+
+            using(MySqlConnection sqlconn = new MySqlConnection(ConfigurationManager.ConnectionStrings["AzureDB"].ConnectionString))
+            {
+                sqlconn.Open();
+                string sql_command = "INSERT INTO parties (partyid,lat,lng) VALUES (@id,@lat,@lng)";
+                MySqlCommand cmd = new MySqlCommand(sql_command, sqlconn);
+                cmd.Parameters.AddWithValue("@id", partyid);
+                cmd.Parameters.AddWithValue("@lat", lat);
+                cmd.Parameters.AddWithValue("@lng", lng);
+                cmd.ExecuteNonQuery();
+                sqlconn.Close();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ DudeFinder/Models/MD5Gen.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+public class MD5Gen
+{
+    public static string ConvertStringtoMD5(string strword)
+    {
+        MD5 md5 = MD5.Create();
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(strword);
+        byte[] hash = md5.ComputeHash(inputBytes);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hash.Length; i++)
+        {
+            sb.Append(hash[i].ToString("x2"));
+        }
+        return sb.ToString();
+    }
+}
