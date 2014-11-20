1f5327d2f9229b548718923e05b395f72e2bad03
Markus Keil <markus@keil-connect.com>
2014-11-20 21:44:18 +0100

    clicking map works to place the target circle

---
+++ DudeFinder/Scripts/dudefinder.js
@@ -0,0 +1,41 @@
+﻿function locate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            var geopos = new L.LatLng(position.coords.latitude, position.coords.longitude);
+
+            window.map.setView(geopos, 15);
+            var marker = L.marker(geopos).addTo(map);
+            marker.bindPopup("You're here!").openPopup();
+        }, function () { $("#locateError").show(); });
+    } else {
+        $("#locateError").show();
+    }
+}
+
+function showCreateParty() {
+    $("#createParty").show();
+    window.map.on("click", function (e) {
+
+        if (window.testLayer) {
+            window.testLayer.removeLayer(createCircle);
+        }
+        window.testLayer = new L.LayerGroup();
+
+        createCircle = L.circle(e.latlng, 20, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5
+        }).addTo(testLayer);
+
+        window.map.addLayer(window.testLayer);
+
+        $("#createParty").data("position", e.latlng);
+        $("#createParty").hide();
+        $("#confirmParty").show();
+    });
+}
+
+function cancelParty() {
+    $("#createParty").hide();
+}
