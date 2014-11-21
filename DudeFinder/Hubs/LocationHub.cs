a565a6999be19ebd8a5816e153cac34def2e9da6
Markus Keil <markus@keil-connect.com>
2014-11-21 22:10:36 +0100

    added signalr to the tracking view

---
+++ DudeFinder/Hubs/LocationHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DudeFinder.Hubs
+{
+    public class LocationHub : Hub
+    {
+        public void UpdateLocation(string partyid,double lat, double lng)
+        {
+            string connid = Context.ConnectionId;
+            Clients.OthersInGroup(partyid).UpdateMember(connid, lat, lng);
+        }
+
+        public async Task JoinParty(string partyid)
+        {
+            await Groups.Add(Context.ConnectionId, partyid);
+            //Clients.OthersInGroup(partyid).PartyJoined(Context.ConnectionId);
+        }
+
+        public async Task LeaveParty(string partyid)
+        {
+            await Groups.Remove(Context.ConnectionId, partyid);
+            //Clients.OthersInGroup(partyid).PartyLeaved(Context.ConnectionId);
+        }
+
+    }
+}
